298ab5c5922ad951a1c759b40c4eb5d6196392ba
Davis <user@localhost>
2023-10-11 20:31:51 +0300

    initial

---
+++ dl22027_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from dl22027_libadd import *
+from dl22027_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
